6267f99f786ae995b413f6b3d3ec2ad0c5366b72
DarkoCBS <darkocbs9@gmail.com>
2024-07-29 02:56:12 +0200

    core
    TODO:
    
    Hold assets on proposal.
    Offchain worker for more complex  (periodic) fundung strategy?

--- pallets/treasury/src/lib.rs
+++ pallets/treasury/src/lib.rs
@@ -16,11 +16,22 @@ mod benchmarking;
 // https://paritytech.github.io/polkadot-sdk/master/frame_support/attr.pallet.html#dev-mode-palletdev_mode
 #[frame_support::pallet(dev_mode)]
 pub mod pallet {
+	use crate::AssetPriceLookup;
+	use frame_support::sp_runtime::traits::AccountIdConversion;
+	use frame_support::traits::tokens::Preservation;
+	use frame_support::PalletId;
 	use frame_support::{
 		pallet_prelude::*,
-		traits::{fungible, fungibles},
+		traits::{
+			fungible::{self, Mutate as FungibleMutate},
+			fungibles::{self, Mutate as FungiblesMutate},
+			EnsureOrigin,
+		},
+		Twox64Concat,
 	};
-	use frame_system::pallet_prelude::*;
+	use frame_system::pallet_prelude::{OriginFor, *};
+
+	const PALLET_ID: PalletId = PalletId(*b"treasury");
 
 	pub type AssetIdOf<T> = <<T as Config>::Fungibles as fungibles::Inspect<
 		<T as frame_system::Config>::AccountId,
@@ -36,6 +47,14 @@ pub mod pallet {
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
+
+	#[pallet::origin]
+	#[derive(PartialEq, Eq, Clone, RuntimeDebug, Encode, Decode, TypeInfo, MaxEncodedLen)]
+	pub enum Origin {
+		SmallSpender,
+		MediumSpender,
+		BigSpender,
+	}
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -53,7 +72,7 @@ pub mod pallet {
 			+ fungible::freeze::Mutate<Self::AccountId>;
 
 		/// Type to access the Assets Pallet.
-		type Fungibles: fungibles::Inspect<Self::AccountId>
+		type Fungibles: fungibles::Inspect<Self::AccountId, Balance = BalanceOf<Self>>
 			+ fungibles::Mutate<Self::AccountId>
 			+ fungibles::Create<Self::AccountId>;
 
@@ -67,22 +86,56 @@ pub mod pallet {
 		// A custom, configurable origin that you can use. It can be wired to be `EnsureSigned`,
 		// `EnsureRoot`, or any custom implementation at the runtime level.
 		// https://paritytech.github.io/polkadot-sdk/master/polkadot_sdk_docs/reference_docs/frame_origin/index.html#asserting-on-a-custom-external-origin
-		type CustomOrigin: EnsureOrigin<Self::RuntimeOrigin>;
+		type TreasuryOrigin: EnsureOrigin<Self::RuntimeOrigin>;
 
 		// Here is an associated type to give you access to your simple asset price lookup function
 		type AssetPriceLookup: crate::AssetPriceLookup<Self>;
 
-		// Small Spender
-		type SmallSpender: EnsureOrigin<Self::RuntimeOrigin>;
+		// type SmallSpender: EnsureOrigin<Self::RuntimeOrigin>;
+		// type BigSpender: EnsureOrigin<Self::RuntimeOrigin>;
+
+		#[pallet::constant]
+		type SmallSpenderThreshold: Get<BalanceOf<Self>>;
+		#[pallet::constant]
+		type MediumSpenderThreshold: Get<BalanceOf<Self>>;
 	}
 
 	/// The pallet's storage items.
 	/// https://paritytech.github.io/polkadot-sdk/master/polkadot_sdk_docs/guides/your_first_pallet/index.html#storage
 	/// https://paritytech.github.io/polkadot-sdk/master/frame_support/pallet_macros/attr.storage.html
+
+	#[derive(TypeInfo, Encode, Decode, MaxEncodedLen)]
+	#[scale_info(skip_type_params(T))]
+	pub struct SpendingProposal<T: Config> {
+		title: [u8; 32],
+		description: [u8; 500],
+		proposer: T::AccountId,
+		beneficiary: T::AccountId,
+		#[codec(compact)]
+		amount: BalanceOf<T>,
+		spender_type: Origin,
+		approved: bool,
+	}
+
 	#[pallet::storage]
-	pub type Something<T> = StorageValue<Value = u32>;
+	pub type SpendingProposals<T: Config> =
+		StorageDoubleMap<_, Twox64Concat, T::AccountId, Twox64Concat, u16, SpendingProposal<T>>;
+
 	#[pallet::storage]
-	pub type SomethingMap<T: Config> = StorageMap<Key = T::AccountId, Value = BlockNumberFor<T>>;
+	pub type NumOfProposalsFromProposer<T: Config> =
+		StorageMap<_, Twox64Concat, T::AccountId, u16, ValueQuery, GetDefault>;
+
+	#[pallet::genesis_config]
+	#[derive(frame_support::DefaultNoBound)]
+	pub struct GenesisConfig;
+
+	#[pallet::genesis_build]
+	impl BuildGenesisConfig for GenesisConfig {
+		fn build(&self) {
+			const PALLET_ID: PalletId = PalletId(*b"treasury");
+			PALLET_ID.try_into_account().expect("Failed to create account ID")
+		}
+	}
 
 	/// Errors inform users that something went wrong.
 	/// https://paritytech.github.io/polkadot-sdk/master/polkadot_sdk_docs/guides/your_first_pallet/index.html#event-and-error
@@ -91,6 +144,13 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		/// We usually use passive tense for events.
 		SomethingStored { something: u32, who: T::AccountId },
+		// index_count is the number of proposals the proposer has made, starting from 0
+		AddedProposal {
+			proposer: T::AccountId,
+			index_count: u16,
+			amount: BalanceOf<T>,
+			title: [u8; 32],
+		},
 	}
 
 	/// Errors inform users that something went wrong.
@@ -122,6 +182,56 @@ pub mod pallet {
 	/// https://paritytech.github.io/polkadot-sdk/master/polkadot_sdk_docs/guides/your_first_pallet/index.html#dispatchables
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		pub fn fund_treasury_asset(
+			origin: OriginFor<T>,
+			amount: BalanceOf<T>,
+			asset_id: AssetIdOf<T>,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			T::Fungibles::transfer(
+				asset_id,
+				&who,
+				&Self::treasury_account_id(),
+				amount,
+				Preservation::Expendable,
+			)?;
+			Ok(())
+		}
+
+		pub fn fund_treasury_native(origin: OriginFor<T>, amount: BalanceOf<T>) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			T::NativeBalance::transfer(
+				&who,
+				&Self::treasury_account_id(),
+				amount,
+				Preservation::Expendable,
+			)?;
+			Ok(())
+		}
+
+		pub fn approve_proposal(
+			origin: OriginFor<T>,
+			proposer: T::AccountId,
+			index: u16,
+		) -> DispatchResult {
+			let _who = T::TreasuryOrigin::ensure_origin(origin)?;
+		
+			SpendingProposals::<T>::try_mutate(&proposer, index, |proposal| {
+				match proposal {
+					Some(p) => {
+						if p.approved {
+							return Err("Proposal already approved");
+						}
+						p.approved = true;
+						Ok(())
+					},
+					None => Err("Proposal does not exist"),
+				}
+			})?;
+		
+			Ok(())
+		}
+
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
@@ -131,7 +241,7 @@ pub mod pallet {
 			let who = ensure_signed(origin)?;
 
 			// Update storage.
-			<Something<T>>::put(something);
+			// <Something<T>>::put(something);
 
 			// Emit an event.
 			Self::deposit_event(Event::SomethingStored { something, who });
@@ -139,31 +249,36 @@ pub mod pallet {
 			Ok(())
 		}
 
-		/// An example dispatchable that may throw a custom error.
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		// /// An example dispatchable that may throw a custom error.
+		// pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
+		// 	let _who = ensure_signed(origin)?;
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
-		}
+		// 	Ok(())
+		// 	// Read a value from storage.
+		// 	match <Something<T>>::get() {
+		// 		// Return an error if the value has not been set.
+		// 		None => Err(Error::<T>::NoneValue.into()),
+		// 		Some(old) => {
+		// 			// Increment the value read from storage; will error in the event of overflow.
+		// 			let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+		// 			// Update the value in storage with the incremented result.
+		// 			<Something<T>>::put(new);
+		// 			Ok(())
+		// 		},
+		// 	}
+		// }
 
 		pub fn propose_spend(
 			origin: OriginFor<T>,
-			who: T::AccountId,
+			title: [u8; 32],
+			description: [u8; 500],
+			asset_id: AssetIdOf<T>,
 			amount: BalanceOf<T>,
+			proposer: T::AccountId,
+			beneficiary: T::AccountId,
 		) -> DispatchResult {
-			T::SmallSpender::ensure_origin(origin)?;
-			Self::do_propose_spend(who, amount)
+			let _anyone = ensure_signed(origin)?;
+			Self::do_propose_spend(title, description, asset_id, amount, proposer, beneficiary)
 		}
 
 		// Let's imagine you wanted to build a transfer extrinsic inside your pallet...
@@ -193,15 +308,75 @@ pub mod pallet {
 	//
 	// Compare this to the block above which has `#[pallet::call]` which makes them extrinsics!
 	impl<T: Config> Pallet<T> {
-		fn do_propose_spend(_who: T::AccountId, amount: BalanceOf<T>) -> DispatchResult {
+		pub fn treasury_account_id() -> T::AccountId {
+			PALLET_ID.try_into_account().expect("Failed to create account ID")
+		}
+
+		pub fn send_native_funds_to_beneficiary(
+			beneficiary: T::AccountId,
+			amount: BalanceOf<T>,
+		) -> DispatchResult {
+			T::NativeBalance::transfer(
+				&Self::treasury_account_id(),
+				&beneficiary,
+				amount,
+				Preservation::Expendable,
+			)?;
+			Ok(())
+		}
+
+		pub fn send_asset_funds_to_beneficiary(
+			beneficiary: T::AccountId,
+			amount: BalanceOf<T>,
+			asset_id: AssetIdOf<T>,
+		) -> DispatchResult {
+			T::Fungibles::transfer(
+				asset_id,
+				&Self::treasury_account_id(),
+				&beneficiary,
+				amount,
+				Preservation::Expendable,
+			)?;
+			Ok(())
+		}
+
+		fn do_propose_spend(
+			title: [u8; 32],
+			description: [u8; 500],
+			asset_id: AssetIdOf<T>,
+			amount: BalanceOf<T>,
+			proposer: T::AccountId,
+			beneficiary: T::AccountId,
+		) -> DispatchResult {
 			// Write the logic for your extrinsic here, since this is "outside" of the macros.
 			// Following this kind of best practice can even allow you to move most of your
 			// pallet logic into different files, with better, more clear structure, rather
 			// than having a single huge complicated file.
-			if amount > 100_000u32.into() {
-				return Err("you are spending too much  money!".into());
-			}
 
+			let price_in_usd = T::AssetPriceLookup::usd_price(asset_id, amount);
+
+			// Determine the spender type based on the amount
+			let spender_type = if price_in_usd <= T::SmallSpenderThreshold::get() {
+				Origin::SmallSpender
+			} else if amount <= T::MediumSpenderThreshold::get() {
+				Origin::MediumSpender
+			} else {
+				Origin::BigSpender
+			};
+
+			let index_count = NumOfProposalsFromProposer::<T>::get(&proposer);
+
+			let proposal = SpendingProposal {
+				title,
+				description,
+				proposer: proposer.clone(),
+				beneficiary,
+				amount,
+				spender_type,
+				approved: false,
+			};
+
+			SpendingProposals::<T>::insert(&proposer, index_count, proposal);
 			Ok(())
 		}
 	}
@@ -217,4 +392,6 @@ pub trait AssetPriceLookup<T: Config> {
 		asset_a_amount: AssetBalanceOf<T>,
 		asset_b_id: AssetIdOf<T>,
 	) -> AssetBalanceOf<T>;
+
+	fn usd_price(asset_id: AssetIdOf<T>, amount: AssetBalanceOf<T>) -> AssetBalanceOf<T>;
 }
